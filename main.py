8a72c62a0c2fb7157ee54fa643fdba7602c6a2d3
Rbn <rubenavalosmtz@gmail.com>
2024-05-29 22:31:57 -0600

    Gravity added
    A gravity simulation was added to the circle drawn on the screen

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import pygame
 
 pygame.init()
 
-screen_size = (800, 600)
-screen = pygame.display.set_mode(screen_size)
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Flappy Bird ;)")
+
+#physics variables:
+speed = 0
+object_x = screen_width//2
+object_y = screen_height//2
+gravity = 5
+dt = 0.2 #measure of the time interval between each update of the simulation's state
+
 
 done = False
 
@@ -17,12 +26,17 @@ while not done:
             done = True
 
     #Game logic
+    speed = speed + gravity*dt
+    object_y = object_y + speed*dt
+
+    if object_y > screen_height:
+        object_y = screen_height
 
     #Drawing logic
     screen.fill((255,255,255)) #just white color
-    pygame.draw.circle(screen,(0,0,255),(400,300), 50)
+    pygame.draw.circle(screen,(0,0,255),(object_x,int(object_y)), 50)
 
-        #update the screen
+    #update the screen
     pygame.display.flip()
 
     clock.tick(60) #fps
