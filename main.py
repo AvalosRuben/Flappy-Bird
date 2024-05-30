49e174cb4be8ecd1f17e56fd7c57e4fcc7aee8b3
Rbn <rubenavalosmtz@gmail.com>
2024-05-29 22:19:12 -0600

    Main cycle of the game
    It contains everything so that the program can start and run

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 import pygame
+
+pygame.init()
+
+screen_size = (800, 600)
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption("Flappy Bird ;)")
+
+done = False
+
+clock = pygame.time.Clock() #to control how fast the display refreshes
+
+while not done:
+    for event in pygame.event.get(): #user did something
+        if event.type == pygame.QUIT:
+            
+            done = True
+
+    #Game logic
+
+    #Drawing logic
+    screen.fill((255,255,255)) #just white color
+    pygame.draw.circle(screen,(0,0,255),(400,300), 50)
+
+        #update the screen
+    pygame.display.flip()
+
+    clock.tick(60) #fps
+
+pygame.quit()
